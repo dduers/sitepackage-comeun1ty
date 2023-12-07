aa3e9d97b0332589ffbc58e3f41b32010720bdaf
Daniel Duersteler <daniel.duersteler@gmail.com>
2023-12-07 22:47:20 +0100

    updated

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,7 +7,7 @@ $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['sitepackage_comeun1ty'] = 'EXT:si
 /**
  * system mails - this is done by the xsite site package
  */
-//$GLOBALS['TYPO3_CONF_VARS']['MAIL']['layoutRootPaths'][700] = 'EXT:sitepackage_comeun1ty/Resources/Private/Layouts/';
+$GLOBALS['TYPO3_CONF_VARS']['MAIL']['layoutRootPaths'][710] = 'EXT:sitepackage_comeun1ty/Resources/Private/Layouts/';
 //$GLOBALS['TYPO3_CONF_VARS']['MAIL']['partialRootPaths'][700] = 'EXT:sitepackage_comeun1ty/Resources/Private/Partials/';
 //$GLOBALS['TYPO3_CONF_VARS']['MAIL']['templateRootPaths'][700] = 'EXT:sitepackage_comeun1ty/Resources/Private/Templates/Email/';
 
